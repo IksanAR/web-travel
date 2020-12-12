1596819493cfdcf4380f686a0cd6b2e85ad69491
Iksan Abdul Rohman <69672850+IksanAR@users.noreply.github.com>
2020-12-12 16:05:00 +0700

    Web-travel laravel

---
+++ resources/views/includes/style.blade.php
@@ -0,0 +1,4 @@
+<link rel="stylesheet" href="{{ url('frontend/libraries/bootstrap/css/bootstrap.css') }}">
+<link href="https://fonts.googleapis.com/css2?family=Assistant:wght@200;300;400;600;700;800&display=swap"
+    rel="stylesheet">
+<link rel="stylesheet" href="{{ url('frontend/style/main.css') }}">
